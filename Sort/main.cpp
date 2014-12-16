1b9fa91487f82fcf23164c5d5ff81d18ff7b5b7f
stef52 <stephenbakalian@gmail.com>
2014-12-15 20:06:27 -0500

    current progress

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -1,48 +1,345 @@
-// MPI C++ "Hello World" program
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
 
-# include <cstdlib>
-# include <iostream>
-# include <iomanip>
-# include <ctime>
+#include "mpi.h"
 
-using namespace std;
+#define InputFilePrefix "./inputFiles/"      //input file location 
+#define OutputFilePrefix "./outputFiles/"    //output file location
+#define ROOT 0
+#define ArrayData int
+#define MPI_Type  MPI_INT
+#define Smaller(x, y) ((x) < (y))
+#define SWAP(x, y) (temp = (x), (x) = (y), (y) = temp)
 
-# include "mpi.h"
-
-
-int main(int argc, char *argv[])
-
+// create an Array of n elements
+ArrayData *MakeArray(int n)
 {
-  int id;
-  int p;
-  double wtime;
-  
-  MPI::Init(argc, argv); //  Initialize MPI.
-  p = MPI::COMM_WORLD.Get_size(); //  Get the number of processes.
-  id = MPI::COMM_WORLD.Get_rank(); //  Get the individual process ID.
+   return((ArrayData *) malloc(n*sizeof(ArrayData)+1));
+};
 
-//  Process 0 prints an introductory message.
-  if (id == 0) 
-  {
-    cout << "\n";
-    cout << "--- HELLO_MPI. I am Master Process 0:\n";
-    cout << "--- The number of processes is " << p << "\n";
-    cout << "\n";
-  }
-//  Every process prints a hello.
-  if (id == 0) 
-  {
-    wtime = MPI::Wtime();
-  }
-  cout << "  Process " << id << " says 'Hello, world!'\n";
-//  Process 0 says goodbye.
-  if (id == 0)
-  {
-    wtime = MPI::Wtime() - wtime;
-    cout << "  Elapsed wall clock time = " << wtime << " seconds.\n";
-  }
-//  Terminate MPI.
-  MPI::Finalize();
-  return 0;
+static void adjust(ArrayData v[], register int m, register int n)
+{
+   register int j, k, temp;
+   ArrayData *b;
+   //origin is 1
+   b = v - 1;  
+   j = m;
+   k = m * 2;
+   while (k <= n)
+   {
+      if (k < n && Smaller(b[k],b[k+1])) ++k;
+      if (Smaller(b[j],b[k]))  SWAP(b[j], b[k]);
+      j = k;
+      k *= 2;
+   }
 }
 
+// sort into increasing order
+void heapsort(ArrayData v[], int n)
+{
+	int j, temp;
+	ArrayData *b;
+	b = v - 1;
+	// put array into heap form
+	for (j = n/2; j > 0; j--)
+		adjust(v, j, n);
+	for (j = n-1; j > 0; j--)
+	{
+	  SWAP(b[1], b[j+1]);
+	  adjust(v, 1, j);
+	}
+}
+
+void sort(ArrayData data[], int localSize, int numprocs)
+{
+  int myid;
+  ArrayData *result;               
+  int resultSize;               
+  ArrayData *splitters, *allSplitters;
+  int i, j, buf, tmp, l, r, L, R;
+  int *scounts, *rcounts, *sdispl, 
+      *rdispl, *bloc, *bsize, *counts, 
+      *send, *receive;
+
+  //MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+  result       = MakeArray((2 * localSize));
+  splitters    = MakeArray(numprocs);
+  allSplitters = MakeArray((numprocs * numprocs));
+  scounts      = (int *) malloc(numprocs*sizeof(int)+1);
+  rcounts      = (int *) malloc(numprocs*sizeof(int)+1);
+  sdispl       = (int *) malloc(numprocs*sizeof(int)+1);
+  rdispl       = (int *) malloc(numprocs*sizeof(int)+1);
+  counts       = (int *) malloc(numprocs*sizeof(int)+1);
+  send         = (int *) malloc(numprocs*sizeof(int)+1);
+  receive      = (int *) malloc(numprocs*sizeof(int)+1);
+  bloc         = (int *) malloc(numprocs*sizeof(int)+1); 
+  bsize        = (int *) malloc(numprocs*sizeof(int)+1); 
+
+  /* ----------------------- compute splitters ----------*/
+  heapsort(data, localSize);
+
+  for (i=0; i<numprocs; i++) {splitters[i] = data[i * localSize / numprocs];};
+  for (i=0; i<numprocs; i++) {rdispl[i] = i * numprocs;};
+  for (i=0; i<numprocs; i++) {rcounts[i] = numprocs;};
+
+  MPI_Gatherv(splitters, numprocs, MPI_Type, allSplitters, rcounts,
+	      rdispl, MPI_INT, 0, MPI_COMM_WORLD);
+
+  if (myid == 0)
+    {
+      heapsort(allSplitters, numprocs * numprocs);
+      for (i=0; i<numprocs; i++) {splitters[i] = allSplitters[i * numprocs];};
+    };
+
+  MPI_Bcast(splitters, numprocs, MPI_Type, 0, MPI_COMM_WORLD);
+
+  /* ---- locate splitters in sorted local data ----------*/
+  j=0;
+  bloc[0] = 0;
+  for (i=1; i<numprocs; i++)
+    {
+      while (Smaller(data[j],splitters[i])) {j++;};
+      bloc[i]=j;
+    }
+  
+
+  /* ---- compute #el. in each sub-bucket ----------*/
+  for (i=0; i<numprocs-1; i++)
+    {
+      bsize[i] = bloc[i+1]-bloc[i];
+    }
+  bsize[numprocs-1] = localSize - bloc[numprocs-1];
+
+  /* ----- communicate bucket sizes ---------------*/
+  for (i=0; i<numprocs; i++)
+    {
+      scounts[i] = 1;
+      sdispl[i] = i;
+      rcounts[i] = 1;
+      rdispl[i] = i;
+    }
+  MPI_Alltoallv(bsize, scounts, sdispl, MPI_INT, counts, rcounts,
+		rdispl,  MPI_Type, MPI_COMM_WORLD);
+
+  /* ----- send buckets to destination proc. ---------------*/
+  resultSize = 1;
+  for (i=0; i<numprocs; i++)
+    {
+      scounts[i] = bsize[i];
+      sdispl[i] =  bloc[i];
+      rcounts[i] = counts[i];
+      rdispl[i] = resultSize-1; resultSize += counts[i];
+    };
+  resultSize--;
+
+  MPI_Alltoallv(data, scounts, sdispl, MPI_Type, result, rcounts,
+		rdispl,  MPI_Type, MPI_COMM_WORLD);
+
+  /* ----- sort data at dest.proc. ---------------*/
+  heapsort(result, resultSize);
+
+
+  /* ----- balance data  ------------------------------------------*/
+
+  /* compute array 'counts' with number of data items at each proc.*/
+  for (i=0; i<numprocs; i++)
+  {
+      rcounts[i] = 1;
+      rdispl[i] = i;
+  };
+
+  MPI_Allgatherv(&resultSize, 1, MPI_INT, counts, rcounts, rdispl,
+		 MPI_INT, MPI_COMM_WORLD);
+
+  /* compute array 'send' with number data items to be sent to each proc.*/
+  L=0;
+  for (i=0; i<myid; i++){L += counts[i];};
+  R = L + counts[myid] - 1;
+
+  for (i=0; i<numprocs; i++)
+  {
+      l = i * localSize;
+      r = ((i+1) * localSize) - 1;
+      send[i] = 0;
+      /*-- L l R r ---*/
+      if ((L <= l) && (l <= R) && (R <= r)) send[i] = R-l+1;
+      /*-- l L r R ---*/
+      if ((l <= L) && (L <= r) && (r <= R)) send[i] = r-L+1;
+      /*-- l L R r ---*/
+      if ((l <= L) && (R <= r)) send[i] = R-L+1;
+      /*-- L l r R ---*/
+      if ((L <= l) && (r <= R)) send[i] = r-l+1;
+  };
+
+  /* compute array 'receive' with number data items to be received each proc.*/
+  for (i=0; i<numprocs; i++)
+  {
+      scounts[i] = 1;
+      sdispl[i] = i;
+      rcounts[i] = 1;
+      rdispl[i] = i;
+  }
+  MPI_Alltoallv(send, scounts, sdispl, MPI_INT, receive, rcounts,
+		rdispl,  MPI_Type, MPI_COMM_WORLD);
+
+  /* move data */
+
+  l = 0;
+  r = 0;
+  for (i=0; i<numprocs; i++)
+    {
+      scounts[i] = send[i];
+      sdispl[i] =  l; l += scounts[i];
+      rcounts[i] = receive[i];
+      rdispl[i] = r; r += rcounts[i];
+    };
+
+  MPI_Alltoallv(result, scounts, sdispl, MPI_Type, data, rcounts,
+		rdispl,  MPI_Type, MPI_COMM_WORLD);
+
+  /* ------ free up memory space ---------------------------------*/
+  free(result);
+  free(splitters);
+  free(allSplitters);
+  free(scounts);
+  free(rcounts);
+  free(sdispl);
+  free(rdispl);
+  free(counts);
+  free(send);
+  free(receive);
+  free(bloc);
+  free(bsize);
+
+};
+/* ============================================================== */
+/* END SORT                                                       */
+/* ============================================================== */
+
+
+/*******************************************************************
+ *                                                                 *
+ *  Calculate and print elapsed time                               *
+ *                                                                 *
+ *******************************************************************/
+
+void printElapsed( char *identString, int myid, int start, int end )
+{
+  int localelapsedTime, globalelapsedTime;
+
+  localelapsedTime = end - start;
+  MPI_Reduce(&localelapsedTime, &globalelapsedTime, 1, MPI_INT,
+	     MPI_MAX, ROOT, MPI_COMM_WORLD);
+
+//  printf("Node %d finished '%s' step: %d sec. elapsed.\n", 
+//        myid, identString, localelapsedTime);
+  if (myid == ROOT)
+    {
+      printf("Maximum time for '%s' step was: %-d sec. elapsed.\n",
+	     identString, globalelapsedTime);
+    }
+  MPI_Reduce(&localelapsedTime, &globalelapsedTime, 1, MPI_INT,
+	     MPI_MIN, ROOT, MPI_COMM_WORLD);
+
+//  printf("Node %d finished '%s' step: %d sec. elapsed.\n", 
+//        myid, identString, localelapsedTime);
+  if (myid == ROOT)
+    {
+      printf("Minimum time for '%s' step was: %-d sec. elapsed.\n",
+	     identString, globalelapsedTime);
+    }
+}
+
+/* ------------- Main ------------------------------------------- */
+
+int main(int argc,char *argv[])
+{
+  int myid, numprocs;
+  FILE *infile, *outfile;
+  char infilepostfix[7], outfilepostfix[8];
+  char infilename[100] = InputFilePrefix;
+  char outfilename[100] = OutputFilePrefix;
+  ArrayData *data;   /* input data */
+  int localSize;     /* input size */
+  int psize;     /* input size */
+
+  int buf, tmp, i, 
+      start, stop, startTotal, stopTotal,
+      startOutput, endOutput,
+      localelapsedTime, globalelapsedTime;
+
+  MPI_Init(&argc, &argv);
+
+  startTotal = MPI_Wtime();
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+  /* ---- read ArraySize and array 'data' from infile ---- */
+  infilepostfix[0] = 'i';
+  infilepostfix[1] = 'n';
+  infilepostfix[2] = 'p';
+  infilepostfix[3] = 'u';
+  infilepostfix[4] = 't';
+  infilepostfix[5] = '-';
+  infilepostfix[6] = ((myid % 100) / 10) + 48;
+  infilepostfix[7] = (myid % 10) + 48;
+  infilepostfix[8] = '.';
+  infilepostfix[9] = 't';
+  infilepostfix[10] = 'x';
+  infilepostfix[11] = 't';
+  infilepostfix[12] = '\0';
+  strcat(infilename, infilepostfix);
+  infile = fopen(infilename, "r");
+  if (infile == NULL) {printf("can't open infile %s \n", infilename);exit(0);};
+  fscanf(infile, "%d\n", &localSize);
+  fscanf(infile, "%d\n", &psize);
+  localSize = localSize/psize;
+  data = MakeArray(localSize);
+  for (i=0; i<localSize; i++) {fscanf(infile, "%d", &buf); data[i]=buf;};
+  tmp = fclose(infile);
+  if (tmp != 0) {printf("can't close infile %s \n", infilename);exit(0);};
+
+  /* --- sort the array ---------------------------------- */
+  start = MPI_Wtime();
+  sort(data, localSize, psize);
+  startOutput = stop = MPI_Wtime();
+
+  /* ---- write array 'data' to outfile ------------------ */
+  outfilepostfix[0] = 'o';
+  outfilepostfix[1] = 'u';
+  outfilepostfix[2] = 't';
+  outfilepostfix[3] = 'p';
+  outfilepostfix[4] = 'u';
+  outfilepostfix[5] = 't';
+  outfilepostfix[6] = ((myid % 100) / 10) + 48;
+  outfilepostfix[7] = (myid % 10) + 48;
+  outfilepostfix[8] = '.';
+  outfilepostfix[9] = 't';
+  outfilepostfix[10] = 'x';
+  outfilepostfix[11] = 't';
+  outfilepostfix[12] = '\0';
+  strcat(outfilename, outfilepostfix);
+  outfile = fopen(outfilename, "w");
+  fprintf(outfile,
+	  "--------- proc. %d: localSize = %d, sort time = %d sec.\n",
+	  myid, localSize, (stop - start));
+  for (i=0; i<localSize; i++) {buf = data[i]; fprintf(outfile,"%d \n", buf);};
+  stopTotal = MPI_Wtime();
+  fprintf(outfile,"--------- Total Time = %d sec.\n\n",
+	  (stopTotal - startTotal) );
+  tmp = fclose(outfile);
+  if (tmp != 0) {printf("can't close outfile %s \n", outfilename);exit(0);};
+
+  endOutput   = MPI_Wtime();
+ // std::string sortChar = "sort";
+ // printElapsed( sortChar,   myid, start,       stop );
+ // printElapsed( "output", myid, startOutput, endOutput );
+  
+  MPI_Finalize();
+
+}
